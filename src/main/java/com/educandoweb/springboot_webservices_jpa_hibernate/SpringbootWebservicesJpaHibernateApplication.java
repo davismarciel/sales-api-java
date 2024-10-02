194535cbdb3bb84953ea48d4d45ff538719663a3
Davi Sá <davisdevi05@gmail.com>
2024-10-02 09:42:43 -0300

    Project created

---
+++ src/main/java/com/educandoweb/springboot_webservices_jpa_hibernate/SpringbootWebservicesJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springboot_webservices_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservicesJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservicesJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/springboot_webservices_jpa_hibernate/SpringbootWebservicesJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.springboot_webservices_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebservicesJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
