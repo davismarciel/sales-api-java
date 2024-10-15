e4f17293c710d9243fb34243f35bb3115f6fc3bd
davismarciel <davisdevi05@gmail.com>
2024-10-15 17:13:54 -0300

    User insert

--- src/main/java/com/springboot_webservices/services/UserService.java
+++ src/main/java/com/springboot_webservices/services/UserService.java
@@ -19,7 +19,12 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
