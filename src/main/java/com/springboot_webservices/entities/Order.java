878868d72c8a4de6fafdb4110376f0a7a97b4596
davismarciel <davisdevi05@gmail.com>
2024-10-15 16:47:09 -0300

    Subtotal and Total methods

--- src/main/java/com/springboot_webservices/entities/Order.java
+++ src/main/java/com/springboot_webservices/entities/Order.java
@@ -46,6 +46,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Double getTotal() {
+		Double sum = 0.0;
+		for (OrderItem item : items) {
+			sum += item.getSubTotal();
+		}
+		return sum;
+	}
+
 	public Long getId() {
 		return id;
 	}
