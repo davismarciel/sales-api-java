9ffff69cfc84553b982e9478846e389bd8b5b577
davismarciel <davisdevi05@gmail.com>
2024-10-21 12:45:21 -0300

    Exception handling - findById

---
+++ src/main/java/com/springboot_webservices/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.springboot_webservices.resources.exceptions;
+
+import com.springboot_webservices.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status, e.getMessage(), error, request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/springboot_webservices/services/UserService.java
+++ src/main/java/com/springboot_webservices/services/UserService.java
@@ -3,6 +3,7 @@ package com.springboot_webservices.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.springboot_webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
